a0d94f6617274ea300883fe723c5777e6e1144b1
Éric Falconnier <eric@zentral.pro>
2021-02-24 21:39:00 +0100

    google_app_engine_application only if cloud func

--- modules/zentral/certbot_cloud_function.tf
+++ modules/zentral/certbot_cloud_function.tf
@@ -87,6 +87,7 @@ resource "time_static" "certbot_schedule_origin" {
 
 # app engine app to unlock scheduler functionality
 resource "google_app_engine_application" "certbot" {
+  count       = var.certbot_cloud_function ? 1 : 0
   project     = data.google_client_config.current.project
   location_id = data.google_client_config.current.region == "us-central1" ? "us-central" : data.google_client_config.current.region
 }
